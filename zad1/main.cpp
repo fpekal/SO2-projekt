e76ddc9ff1536edec6d76754c175830b70df6eec
Filip Pękala <272985@student.pwr.edu.pl>
2025-03-04 09:58:35 +0100

    Add arguments to the program

--- zad1/main.cpp
+++ zad1/main.cpp
@@ -2,8 +2,14 @@
 #include "problem.h"
 #include "printer.h"
 
-int main() {
-	Problem p { 3 };
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << " <number>" << std::endl;
+		return 1;
+	}
+
+	int n = std::stoi(argv[1]);
+	Problem p { n };
 
 	std::this_thread::sleep_for(std::chrono::seconds(10));
 	return 0;
