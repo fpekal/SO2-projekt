5b19cfb5225413bd2c129ba7d32da7e88ff69021
Filip Pękala <272985@student.pwr.edu.pl>
2025-03-04 08:39:41 +0100

    Comments

--- zad1/problem.h
+++ zad1/problem.h
@@ -23,6 +23,7 @@ public:
 	}
 	
 	~Problem() {
+		// Close all philosophers' threads
 		running = false;
 
 		for (int i = 0; i < phil_num; i++) {
@@ -30,48 +31,67 @@ public:
 		}
 	}
 
+	// How many forks (for eating) there are
 	int fork_num;
+
+	// How many philosophers there are. This number should be equal to the number of forks
 	int phil_num;
 
+	// Fork can be used by only one philosopher at a time, so it's modeled as a mutex
 	std::vector<std::shared_ptr<std::mutex>> forks;
+
 	std::vector<std::thread> philosophers;
 
+
+	// Arbiter is used to coordinate philosophers
+	// It makes sure that only N-1 philosophers try to eat at the same time
 	int arbiter;
 	std::mutex arbiter_mutex;
+
+	// Notifies philosophers that there is a new slot for new philosopher to eat
 	std::condition_variable arbiter_cv;
 
+	// Thread-Safe printing to console
 	Printer p;
 
+	// Is the problem still running
 	std::atomic<bool> running;
 
 private:
 	static void run_philosopher(Problem* problem, int index) {
+		// Get neighboring forks
 		auto left_fork = problem->forks[index];
 		auto right_fork = problem->forks[(index + 1) % problem->fork_num];
 
 		while (problem->running) {
 			problem->p << std::string{"Philosopher "} + std::to_string(index) + " tries to eat\n";
+			// Create locks but don't lock them
 			std::unique_lock<std::mutex> guard(*left_fork, std::defer_lock);
 			std::unique_lock<std::mutex> guard2(*right_fork, std::defer_lock);
 
 			{
+				// Wait for arbitrator to allow philosopher to eat
 				std::unique_lock<std::mutex> arbiter_guard(problem->arbiter_mutex);
 				problem->arbiter_cv.wait(arbiter_guard, [&] { return problem->arbiter > 0; });
 				problem->arbiter--;
-				problem->p << std::string{"Philosopher "} + std::to_string(index) + " is eating\n";
 				arbiter_guard.unlock();
 
+				// Take forks and eat for some time
+				problem->p << std::string{"Philosopher "} + std::to_string(index) + " is eating\n";
 				guard.lock();
 				guard2.lock();
 				std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 500 + 500));
 				guard.unlock();
 				guard2.unlock();
-
 				problem->p << std::string{"Philosopher "} + std::to_string(index) + " is no longer eating\n";
+
+				// Return slot and notify other philosophers that there is a new slot for them
 				arbiter_guard.lock();
 				problem->arbiter++;
 				problem->arbiter_cv.notify_one();
 				arbiter_guard.unlock();
+
+				// Think
 				std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 500 + 1000));
 			}
 		}
