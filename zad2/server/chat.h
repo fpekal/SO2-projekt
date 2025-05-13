308d4cfbf6d6f3c90d721fe534666e29529629dd
Filip Pękala <272985@student.pwr.edu.pl>
2025-05-13 23:05:00 +0200

    Make trimming of whitespaces better

--- zad2/server/chat.h
+++ zad2/server/chat.h
@@ -9,6 +9,30 @@
 
 #include "server.h"
 
+// Trim from start (in-place)
+static std::string ltrim(std::string s) {
+  s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+            return !std::isspace(ch);
+          }));
+  return s;
+}
+
+// Trim from end (in-place)
+static std::string rtrim(std::string s) {
+  s.erase(std::find_if(s.rbegin(), s.rend(),
+                       [](unsigned char ch) { return !std::isspace(ch); })
+              .base(),
+          s.end());
+
+  return s;
+}
+
+// Trim from both ends (in-place)
+static std::string trim(std::string s) {
+  s = ltrim(s);
+  return rtrim(s);
+}
+
 /** @class Client
  * @brief Chat client
  *
@@ -185,7 +209,7 @@ private:
   void add_history(const std::string &message) {
     std::unique_lock<std::mutex> lock{history_mutex};
 
-    history += message.substr(0, message.length() - 2);
+    history += trim(message);
     history += "\n";
   }
 
