53a7b83662e68873e1e563426e85c89027606355
Filip Pękala <272985@student.pwr.edu.pl>
2025-05-13 21:56:29 +0200

    Add default port and address

--- zad2/client/main.cpp
+++ zad2/client/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+int DEFAULT_PORT = 2137;
+std::string DEFAULT_ADDRESS = "127.0.0.1";
+
 int main(int argc, char *argv[]) {
   std::cout << "Hello, World! Client!";
   return 0;
