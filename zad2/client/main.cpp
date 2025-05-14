fea61bb5075a048a84577480cae6d76e5aa3650f
Filip Pękala <272985@student.pwr.edu.pl>
2025-05-14 14:19:30 +0200

    Add docs to the client

--- zad2/client/main.cpp
+++ zad2/client/main.cpp
@@ -12,12 +12,31 @@
 int DEFAULT_PORT = 2137;
 std::string DEFAULT_ADDRESS = "127.0.0.1";
 
+/** @class Client
+ * @brief Chat client
+ *
+ * Connects to the server. Allows to send and receive messages.
+ * Messages are just plaintext ASCII-encoded strings containing both nickname
+ * and a "message".
+ */
 class Client {
 public:
+  /**
+   * @brief Constructs a Client object.
+   *
+   * @param port Port to which the client will connect.
+   * @param address IP address of the server.
+   */
   Client(int port, const std::string &address)
       : port{port}, address{address}, fd{0} {}
   ~Client() { close(fd); }
 
+  /** @fn void connect()
+   * @brief Connect to the server.
+   *
+   * Creates a socket `fd` and connects it to the server specified in the
+   * constructor.
+   */
   void connect() {
     fd = socket(AF_INET, SOCK_STREAM, 0);
     assert(fd > 0);
@@ -33,10 +52,24 @@ public:
     }
   }
 
+  /** @fn void send(const std::string &message)
+   * @brief Send a message
+   *
+   * Sends a message.
+   * The message should already contain the user's nickname.
+   *
+   * @param message Message to send
+   */
   void send(const std::string &message) {
     ::send(fd, message.c_str(), message.length(), 0);
   }
 
+  /** @fn std::string receive()
+   * @brief Receive a message
+   *
+   * Blocks until new message appears from the server.
+   * Then it receives it and returns as a std::string.
+   */
   std::string receive() {
     std::string message;
     std::vector<char> buffer;
@@ -64,7 +97,7 @@ private:
   std::string address;
 };
 
-void receiver_thread_func(Client &c, std::atomic<bool> &running) {
+static void receiver_thread_func(Client &c, std::atomic<bool> &running) {
   while (running) {
     std::cout << c.receive();
   }
