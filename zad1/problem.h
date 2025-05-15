4e959aa6761cdd071911530acbe46b21357ae342
Filip Pękala <272985@student.pwr.edu.pl>
2025-05-15 09:22:01 +0200

    Fix the code and reformat

--- zad1/problem.h
+++ zad1/problem.h
@@ -1,99 +1,107 @@
 #pragma once
-#include <vector>
-#include <thread>
-#include <mutex>
-#include <memory>
-#include <condition_variable>
 #include <atomic>
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 #include "printer.h"
 
 class Problem {
 public:
-	Problem(int phil_num) :
-		phil_num{ phil_num }, fork_num{ phil_num }, arbiter{ phil_num - 1 }, running{ true } {
+  Problem(int phil_num)
+      : fork_num{phil_num}, phil_num{phil_num}, arbiter{phil_num - 1},
+        running{true} {
 
-		for (int i = 0; i < fork_num; i++) {
-			forks.push_back(std::make_shared<std::mutex>());
-		}
-		
-		for (int i = 0; i < phil_num; i++) {
-			philosophers.push_back(std::thread(&Problem::run_philosopher, this, i));
-		}
-	}
-	
-	~Problem() {
-		// Close all philosophers' threads
-		running = false;
+    for (int i = 0; i < fork_num; i++) {
+      forks.push_back(std::make_shared<std::mutex>());
+    }
 
-		for (int i = 0; i < phil_num; i++) {
-			philosophers[i].join();
-		}
-	}
+    for (int i = 0; i < phil_num; i++) {
+      philosophers.push_back(std::thread(&Problem::run_philosopher, this, i));
+    }
+  }
 
-	// How many forks (for eating) there are
-	int fork_num;
+  ~Problem() {
+    // Close all philosophers' threads
+    running = false;
 
-	// How many philosophers there are. This number should be equal to the number of forks
-	int phil_num;
+    for (int i = 0; i < phil_num; i++) {
+      philosophers[i].join();
+    }
+  }
 
-	// Fork can be used by only one philosopher at a time, so it's modeled as a mutex
-	std::vector<std::shared_ptr<std::mutex>> forks;
+  // How many forks (for eating) there are
+  int fork_num;
 
-	std::vector<std::thread> philosophers;
+  // How many philosophers there are. This number should be equal to the number
+  // of forks
+  int phil_num;
 
+  // Fork can be used by only one philosopher at a time, so it's modeled as a
+  // mutex
+  std::vector<std::shared_ptr<std::mutex>> forks;
 
-	// Arbiter is used to coordinate philosophers
-	// It makes sure that only N-1 philosophers try to eat at the same time
-	int arbiter;
-	std::mutex arbiter_mutex;
+  std::vector<std::thread> philosophers;
 
-	// Notifies philosophers that there is a new slot for a new philosopher to eat
-	std::condition_variable arbiter_cv;
+  // Arbiter is used to coordinate philosophers
+  // It makes sure that only N-1 philosophers try to eat at the same time
+  int arbiter;
+  std::mutex arbiter_mutex;
 
-	// Thread-Safe printing to the console
-	Printer p;
+  // Notifies philosophers that there is a new slot for a new philosopher to eat
+  std::condition_variable arbiter_cv;
 
-	// Is the problem still running
-	std::atomic<bool> running;
+  // Thread-Safe printing to the console
+  Printer p;
+
+  // Is the problem still running
+  std::atomic<bool> running;
 
 private:
-	void run_philosopher(int index) {
-		// Get neighboring forks
-		auto left_fork = forks[index];
-		auto right_fork = forks[(index + 1) % fork_num];
+  void run_philosopher(int index) {
+    // Get neighboring forks
+    auto left_fork = forks[index];
+    auto right_fork = forks[(index + 1) % fork_num];
 
-		while (running) {
-			p << std::string{"Philosopher "} + std::to_string(index) + " tries to eat\n";
-			// Create locks but don't lock them
-			std::unique_lock<std::mutex> guard(*left_fork, std::defer_lock);
-			std::unique_lock<std::mutex> guard2(*right_fork, std::defer_lock);
+    while (running) {
+      p << std::string{"Philosopher "} + std::to_string(index) +
+               " tries to eat\n";
+      // Create locks but don't lock them
+      std::unique_lock<std::mutex> guard(*left_fork, std::defer_lock);
+      std::unique_lock<std::mutex> guard2(*right_fork, std::defer_lock);
 
-			{
-				// Wait for arbitrator to allow the philosopher to eat
-				std::unique_lock<std::mutex> arbiter_guard(arbiter_mutex);
-				arbiter_cv.wait(arbiter_guard, [&] { return arbiter > 0; });
-				arbiter--;
-				arbiter_guard.unlock();
+      {
+        // Wait for arbitrator to allow the philosopher to eat
+        std::unique_lock<std::mutex> arbiter_guard(arbiter_mutex);
+        arbiter_cv.wait(arbiter_guard, [&] { return arbiter > 0; });
+        arbiter--;
+        arbiter_guard.unlock();
 
-				// Take forks and eat for some time
-				p << std::string{"Philosopher "} + std::to_string(index) + " is eating\n";
-				guard.lock();
-				guard2.lock();
-				std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 500 + 500));
-				guard2.unlock();
-				guard.unlock();
-				p << std::string{"Philosopher "} + std::to_string(index) + " is no longer eating\n";
+        // Take forks and eat for some time
+        p << std::string{"Philosopher "} + std::to_string(index) +
+                 " is eating\n";
+        guard.lock();
+        guard2.lock();
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(std::rand() % 500 + 500));
+        guard2.unlock();
+        guard.unlock();
+        p << std::string{"Philosopher "} + std::to_string(index) +
+                 " is no longer eating\n";
 
-				// Return the slot and notify other philosophers that there is a new slot for them
-				arbiter_guard.lock();
-				arbiter++;
-				arbiter_cv.notify_one();
-				arbiter_guard.unlock();
+        // Return the slot and notify other philosophers that there is a new
+        // slot for them
+        arbiter_guard.lock();
+        arbiter++;
+        arbiter_cv.notify_one();
+        arbiter_guard.unlock();
 
-				// Think
-				std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 500 + 1000));
-			}
-		}
-	}
+        // Think
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(std::rand() % 500 + 1000));
+      }
+    }
+  }
 };
